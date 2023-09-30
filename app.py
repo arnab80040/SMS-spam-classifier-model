fad5b096199e49143b65f1cca65f3b7a2673d4dc
Arnab Sadhu <119506408+arnab80040@users.noreply.github.com>
2023-10-01 01:10:51 +0530

    app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import nltk
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
 nltk.download('punkt')
+nltk.download('corpora')
+nltk.download('corpora/stopwords')
 nltk.download('stopwords')
 
 ps = PorterStemmer()
